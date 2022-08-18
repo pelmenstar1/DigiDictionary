60872f2ad5eaf7897edb7f942d4cf8ec589b7c40
pelme <pelmenstar1@gmail.com>
2022-08-19 00:08:15 +0300

    Fix

--- app/src/main/java/io/github/pelmenstar1/digiDict/ui/manageRecordBadges/ManageRecordBadgesFragment.kt
+++ app/src/main/java/io/github/pelmenstar1/digiDict/ui/manageRecordBadges/ManageRecordBadgesFragment.kt
@@ -40,6 +40,7 @@ class ManageRecordBadgesFragment : Fragment() {
 
         vm.onRemoveError.handler = showSnackbarEventHandler(container, R.string.manageRecordBadges_removeError)
         vm.onAddError.handler = showSnackbarEventHandler(container, R.string.manageRecordBadges_addError)
+        vm.onEditError.handler = showSnackbarEventHandler(container, R.string.manageRecordBadges_editError)
 
         binding.manageRecordBadgesRecyclerView.also {
             it.adapter = adapter
