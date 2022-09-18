7659f44a9c97764c056aecaa97061b2052cb9e82
pelme <pelmenstar1@gmail.com>
2022-09-18 21:19:43 +0300

    Cache padding and text appearance in BadgeSelectorDialogAdapter

--- app/src/main/java/io/github/pelmenstar1/digiDict/ui/addEditRecord/badge/BadgeSelectorDialogAdapter.kt
+++ app/src/main/java/io/github/pelmenstar1/digiDict/ui/addEditRecord/badge/BadgeSelectorDialogAdapter.kt
@@ -4,12 +4,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.setPadding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
 import io.github.pelmenstar1.digiDict.R
-import io.github.pelmenstar1.digiDict.common.ui.setPaddingRes
-import io.github.pelmenstar1.digiDict.common.ui.setTextAppearance
+import io.github.pelmenstar1.digiDict.common.getLazyValue
+import io.github.pelmenstar1.digiDict.common.textAppearance.TextAppearance
 import io.github.pelmenstar1.digiDict.data.RecordBadgeInfo
 
 class BadgeSelectorDialogAdapter(
@@ -49,6 +50,8 @@ class BadgeSelectorDialogAdapter(
     }
 
     private var elements = emptyList<RecordBadgeInfo>()
+    private var elementPadding: Int = -1
+    private var elementTextAppearance: TextAppearance? = null
 
     fun submitData(elements: List<RecordBadgeInfo>) {
         val result = DiffUtil.calculateDiff(Callback(this.elements, elements))
@@ -60,11 +63,24 @@ class BadgeSelectorDialogAdapter(
     override fun getItemCount() = elements.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val textAppearance = getLazyValue(
+            elementTextAppearance,
+            { TextAppearance(context) { BodyLarge } },
+            { elementTextAppearance = it }
+        )
+
+        var padding = elementPadding
+        if (padding < 0) {
+            padding = context.resources.getDimensionPixelOffset(R.dimen.badgeSelectorDialog_listElementPadding)
+            elementPadding = padding
+        }
+
         val textView = MaterialTextView(parent.context).apply {
             layoutParams = ITEM_LAYOUT_PARAMS
 
-            setPaddingRes(R.dimen.badgeSelectorDialog_listElementPadding)
-            setTextAppearance { BodyLarge }
+            setPadding(padding)
+            textAppearance.apply(this)
         }
 
         return ViewHolder(textView)
