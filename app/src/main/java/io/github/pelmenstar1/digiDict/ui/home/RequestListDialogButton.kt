969251902b9213e254922117d563bb43ade8b8d2
pelmenstar1 <pelmenstar1@gmail.com>
2023-01-01 02:16:54 +0200

    Add request sort list dialog button to home screen

---
+++ app/src/main/java/io/github/pelmenstar1/digiDict/ui/home/RequestListDialogButton.kt
@@ -0,0 +1,82 @@
+package io.github.pelmenstar1.digiDict.ui.home
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.button.MaterialButton
+import io.github.pelmenstar1.digiDict.R
+import io.github.pelmenstar1.digiDict.common.android.MaxRoundRectDrawable
+
+/**
+ * Represents a button that looks like a spinner but is actually a button which on click shows dialog with list where
+ * the user can select an option.
+ */
+class RequestListDialogButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = com.google.android.material.R.attr.materialButtonStyle,
+    @StyleRes defStyleRes: Int = 0
+) : MaterialButton(context, attrs, defStyleAttr) {
+    var prefix: String = ""
+        set(value) {
+            field = value
+
+            updateText()
+        }
+
+    var value: String = ""
+        set(value) {
+            field = value
+
+            updateText()
+        }
+
+    init {
+        val res = context.resources
+        val theme = context.theme
+
+        background = MaxRoundRectDrawable().apply {
+            color = ResourcesCompat.getColor(res, R.color.request_list_dialog_button_background, theme)
+        }
+        backgroundTintList = null
+
+        icon = ResourcesCompat.getDrawable(res, R.drawable.ic_arrow_down, theme)
+        iconGravity = ICON_GRAVITY_END
+        iconTint = ResourcesCompat.getColorStateList(res, R.color.request_list_dialog_button_icon_tint, theme)
+        iconPadding = res.getDimensionPixelOffset(R.dimen.home_requestListDialogButton_iconPadding)
+
+        res.getDimensionPixelOffset(R.dimen.home_requestListDialogButton_verticalPadding).let {
+            setPadding(0, it, 0, it)
+        }
+        minimumHeight = 0
+        minHeight = 0
+
+        if (attrs != null) {
+            val a =
+                context.obtainStyledAttributes(attrs, R.styleable.RequestListDialogButton, defStyleAttr, defStyleRes)
+
+            try {
+                a.getString(R.styleable.RequestListDialogButton_prefix)?.let { prefix = it }
+            } finally {
+                a.recycle()
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateText() {
+        val prefix = prefix
+        val value = value
+
+        val buffer = CharArray(prefix.length + value.length + 2)
+        prefix.toCharArray(buffer, destinationOffset = 0)
+        buffer[prefix.length] = ':'
+        buffer[prefix.length + 1] = ' '
+        value.toCharArray(buffer, destinationOffset = prefix.length + 2)
+
+        text = String(buffer)
+    }
+}
