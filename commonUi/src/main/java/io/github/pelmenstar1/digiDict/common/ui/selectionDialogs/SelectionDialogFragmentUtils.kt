00d9271b918031b654f7b9fc3559c34509b25084
pelmenstar1 <pelmenstar1@gmail.com>
2023-04-16 17:02:05 +0300

    Remove some unused code

--- commonUi/src/main/java/io/github/pelmenstar1/digiDict/common/ui/selectionDialogs/SelectionDialogFragmentUtils.kt
+++ commonUi/src/main/java/io/github/pelmenstar1/digiDict/common/ui/selectionDialogs/SelectionDialogFragmentUtils.kt
@@ -1,13 +1,11 @@
 package io.github.pelmenstar1.digiDict.common.ui.selectionDialogs
 
 import android.content.Context
-import android.widget.CompoundButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.StringRes
 import androidx.core.widget.TextViewCompat
 import com.google.android.material.textview.MaterialTextView
-import io.github.pelmenstar1.digiDict.common.textAppearance.TextAppearance
 import io.github.pelmenstar1.digiDict.common.ui.R
 
 internal object SelectionDialogFragmentUtils {
@@ -24,40 +22,6 @@ internal object SelectionDialogFragmentUtils {
 
             text = res.getText(titleRes)
             TextViewCompat.setTextAppearance(this, R.style.TextAppearance_DigiDictionary_SelectionDialog_Title)
-        }
-    }
-
-    inline fun createAndAddViewsForItems(
-        context: Context,
-        choices: Array<out String>,
-        root: LinearLayout,
-        itemOnCheckedChangedListener: CompoundButton.OnCheckedChangeListener,
-        createView: () -> CompoundButton,
-        isChoiceChecked: (index: Int) -> Boolean
-    ) {
-        val res = context.resources
-
-        val textVerticalPadding = res.getDimensionPixelOffset(R.dimen.selectionDialog_textVerticalPadding)
-        val textHorizontalPadding = res.getDimensionPixelOffset(R.dimen.selectionDialog_textHorizontalPadding)
-        val textAppearance = TextAppearance(context) { BodyLarge }
-
-        val itemLayoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-
-        for ((index, choice) in choices.withIndex()) {
-            root.addView(createView().apply {
-                layoutParams = itemLayoutParams
-                setPadding(textHorizontalPadding, textVerticalPadding, textHorizontalPadding, textVerticalPadding)
-
-                tag = index
-                text = choice
-                isChecked = isChoiceChecked(index)
-                textAppearance.apply(this)
-
-                setOnCheckedChangeListener(itemOnCheckedChangedListener)
-            })
         }
     }
 }
